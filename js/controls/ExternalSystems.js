88ce0a803b9225e22ac9af38d4c243e11db980fc
Sovann20 <sovannchak20@live.com>
2018-11-20 09:21:28 -0600

    Some edits before I merge Sam's code.

---
+++ js/controls/ExternalSystems.js
@@ -0,0 +1,103 @@
+/*
+ * Outputs the external system's states.
+ * @author Sam Guenette and Sovann Chak
+ * 
+ */
+            let level = 100;
+            let oxygenLevel = 100 
+            let oClass = `<i class="fa fa-battery-full" style="color:green"></i>`;
+            let batteryClass = `<i class="fa fa-battery-full" style="color:green"></i>`;
+
+            //document.getElementById("batteryDisplay").innerHTML = level   
+            function batteryTimer(){
+
+                if(level>0)
+                {   
+                    batteryClass = `<i class="fa fa-battery-full" style="color:green"></i>`;
+
+                    if(level>=85)
+                    {   
+                        batteryClass = `<i class="fa fa-battery-4" style="color:green"></i>`;
+                    }   
+                    else if(level>=70 && level <85)
+                    {   
+                        batteryClass = `<i class="fa fa-battery-three-quarters" style="color:green"></i>`;
+                    }   
+                    else if(level>=60 && level <70)
+                    {   
+                        batteryClass = `<i class="fa fa-battery-3" style="color:green"></i>`;
+                    }   
+                    else if(level>=50 && level <60)
+                    {   
+                        batteryClass = `<i class="fa fa-battery-half" style="color:yellow"></i>`;
+                    }   
+                    else if(level>=35 && level<50)
+                    {   
+                        batteryClass = `<i class="fa fa-battery-2" style="color:yellow"></i>`;
+                    }   
+                    else if(level>=15 && level<35)
+                    {   
+                        batteryClass = `<i class="fa fa-battery-quarter" style="color:red"></i>`;
+                    }   
+                    else
+                    {   
+                        batteryClass = `<i class="fa fa-battery-0" style="color:red"></i>`;
+                    }   
+
+                    level--;
+                    document.getElementById("batteryDisplay").innerHTML ="Battery: " + level + "%" + batteryClass;
+                    oxygenTimer();
+                }   
+                else
+                {   
+                    document.getElementById("oxygenDisplay").innerHTML ="Battery: 0%";
+                    alert("OUT OF Battery");
+                }   
+            }   
+
+            function oxygenTimer(){
+                if(oxygenLevel>0)
+                {   
+
+                    oClass = `<i class="fa fa-battery-full" style="color:green"></i>`;
+
+                    if(oxygenLevel>=85)
+                    {   
+                        oClass = `<i class="fa fa-battery-4" style="color:green"></i>`;
+                    }
+                    else if(oxygenLevel>=70 && oxygenLevel<85)
+                    {
+                        oClass = `<i class="fa fa-battery-three-quarters" style="color:green"></i>`;
+                    }
+                    else if(oxygenLevel>=60 && oxygenLevel<70)
+                    {
+                        oClass = `<i class="fa fa-battery-3" style="color:green"></i>`
+                    }
+                    else if(oxygenLevel>=50 && oxygenLevel<60)
+                    {
+                        oClass = `<i class="fa fa-battery-half" style="color:yellow"></i>`
+                    }
+                    else if(oxygenLevel>=35 && oxygenLevel<50)
+                    {
+                        oClass = `<i class="fa fa-battery-2" style="color:yellow"></i>`
+                    }
+                    else if(oxygenLevel>=15 && oxygenLevel<35)
+                    {
+                        oClass = `<i class="fa fa-battery-quarter" style="color:red"></i>`
+                    }
+                    else
+                    {
+                        oClass = `<i class="fa fa-battery-0" style="color:red"></i>`
+                    }
+
+                    document.getElementById("oxygenDisplay").innerHTML ="Oxygen: " + oxygenLevel + "%" + oClass;
+                    oxygenLevel = oxygenLevel -2.5;
+                }
+                else{
+                    document.getElementById("oxygenDisplay").innerHTML ="Oxygen: 0%";
+                    alert("OUT OF Oxygen");
+                }
+            }
+
+            setInterval(batteryTimer, 10000);
+
--- js/controls/FlyControls.js
+++ js/controls/FlyControls.js
@@ -3,107 +3,6 @@
  * ----
  * Control updates by Jacob Perin
  */
-//Timer for battery and oxygen
-
-/*timer for oxygen and battery based inside a timer*/
-			let level = 100;
-			let oxygenLevel = 100
-			let oClass = `<i class="fa fa-battery-full" style="font-size:48px;color:green"></i>`;
-			let batteryClass = `<i class="fa fa-battery-full" style="font-size:48px;color:green"></i>`;
-
-			//document.getElementById("batteryDisplay").innerHTML = level	
-			function batteryTimer(){
-
-				if(level>0)
-				{
-					batteryClass = `<i class="fa fa-battery-full" style="font-size:48px;color:green"></i>`;
-
-					if(level>=85)
-					{
-						batteryClass = `<i class="fa fa-battery-4" style="font-size:48px;color:green"></i>`;
-					}
-					else if(level>=70 && level <85)
-					{
-						batteryClass = `<i class="fa fa-battery-three-quarters" style="font-size:48px;color:green"></i>`;
-					}
-					else if(level>=60 && level <70)
-					{
-						batteryClass = `<i class="fa fa-battery-3" style="font-size:48px;color:green"></i>`;
-					}
-					else if(level>=50 && level <60)
-					{
-						batteryClass = `<i class="fa fa-battery-half" style="font-size:48px;color:yellow"></i>`;
-					}
-					else if(level>=35 && level<50)
-					{
-						batteryClass = `<i class="fa fa-battery-2" style="font-size:48px;color:yellow"></i>`;
-					}
-					else if(level>=15 && level<35)
-					{
-						batteryClass = `<i class="fa fa-battery-quarter" style="font-size:48px;color:red"></i>`;
-					}
-					else
-					{
-						batteryClass = `<i class="fa fa-battery-0" style="font-size:48px;color:red"></i>`;
-					}
-
-					level--;
-					document.getElementById("batteryDisplay").innerHTML ="Battery:" + level + "%" + batteryClass;
-					oxygenTimer();
-				}
-				else
-				{
-					document.getElementById("oxygenDisplay").innerHTML ="Oxygen: 0%";
-					alert("OUT OF Battery");
-				}
-			}
-
-			function oxygenTimer(){
-				if(oxygenLevel>0)
-				{
-
-					oClass = `<i class="fa fa-battery-full" style="font-size:48px;color:green"></i>`;
-
-					if(oxygenLevel>=85)
-					{
-						oClass = `<i class="fa fa-battery-4" style="font-size:48px;color:green"></i>`;
-					}
-					else if(oxygenLevel>=70 && oxygenLevel<85)
-					{
-						oClass = `<i class="fa fa-battery-three-quarters" style="font-size:48px;color:green"></i>`;
-					}
-					else if(oxygenLevel>=60 && oxygenLevel<70)
-					{
-						oClass = `<i class="fa fa-battery-3" style="font-size:48px;color:green"></i>`
-					}
-					else if(oxygenLevel>=50 && oxygenLevel<60)
-					{
-						oClass = `<i class="fa fa-battery-half" style="font-size:48px;color:yellow"></i>`
-					}
-					else if(oxygenLevel>=35 && oxygenLevel<50)
-					{
-						oClass = `<i class="fa fa-battery-2" style="font-size:48px;color:yellow"></i>`
-					}
-					else if(oxygenLevel>=15 && oxygenLevel<35)
-					{
-						oClass = `<i class="fa fa-battery-quarter" style="font-size:48px;color:red"></i>`
-					}
-					else
-					{
-						oClass = `<i class="fa fa-battery-0" style="font-size:48px;color:red"></i>`
-					}
-
-					document.getElementById("oxygenDisplay").innerHTML ="Oxygen:" + oxygenLevel + "%" + oClass;
-					oxygenLevel = oxygenLevel -2.5;
-				}
-				else{
-					document.getElementById("oxygenDisplay").innerHTML ="Oxygen: 0%";
-					alert("OUT OF Oxygen");
-				}
-			}
-
-			setInterval(batteryTimer, 10000);
-//-----------------------------------------------------------------------------------------------------------------------------
 
 //This value is subject to change
 var _fuel = 10002;
@@ -397,24 +296,24 @@ function fuelDegradation() {
     var currentPercent = Math.floor((_fuel/10000)*100);
     if(_fuel >= 7500) {
         document.getElementById('fuel-info-bar').style.color = "green";
-        document.getElementById('fuel-info-state').innerHTML = "Fuel level is good: "+currentPercent+"%";
+        document.getElementById('fuel-info-state').innerHTML = "Fuel level: "+currentPercent+"%";
     }
     else if(_fuel >= 5000) { 
         //yellow - halfway
         document.getElementById('fuel-info-bar').style.color = "yellow";
         document.getElementById('fuel-info-bar').className = "fas fa-thermometer-half";
-        document.getElementById('fuel-info-state').innerHTML = "Fuel level is sufficent: "+currentPercent+"%";
+        document.getElementById('fuel-info-state').innerHTML = "Fuel level: "+currentPercent+"%";
     }
     else if(_fuel >= 1) {
         //red - critical
         document.getElementById('fuel-info-bar').style.color = "red";
         document.getElementById('fuel-info-bar').className = "fas fa-thermometer-empty";
-        document.getElementById('fuel-info-state').innerHTML = "Fuel level is low: "+currentPercent+"%";
+        document.getElementById('fuel-info-state').innerHTML = "Fuel level low: "+currentPercent+"%";
     } 
     else {
         document.getElementById('fuel-info-bar').style.color = "red";
         document.getElementById('fuel-info-bar').className = "fas fa-thermometer-empty";
-        document.getElementById('fuel-info-state').innerHTML = "Fuel level is empty.";
+        document.getElementById('fuel-info-state').innerHTML = "Fuel is empty.";
     }
 };
 
